c3944d61d305f8fbbe95e547812e6a69c5662319
lukaszgo3 <lukaszgo3@gmail.com>
2017-05-10 15:49:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/tourguide/FragmentAdapter.java
@@ -0,0 +1,54 @@
+package com.example.android.tourguide;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public FragmentAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return new Fragment1();
+            case 1:
+                return new Fragment2();
+            case 2:
+                return new Fragment3();
+            case 3:
+                return new Fragment4();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.fragment1);
+            case 1:
+                return mContext.getString(R.string.fragment2);
+            case 2:
+                return mContext.getString(R.string.fragment3);
+            case 3:
+                return mContext.getString(R.string.fragment4);
+            default:
+                return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.tourguide;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        FragmentAdapter adapter = new FragmentAdapter(this, getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabsId);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+}
